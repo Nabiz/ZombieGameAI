6433e8e6636be5ba24cf1a2d94b61f4d2c7f8efc
Nabiz <dawcio.gwizdz@gmail.com>
2022-10-24 20:22:28 +0200

    add basic steering bahaviors

--- ZombieGame/SteeringBehaviors.gd
+++ ZombieGame/SteeringBehaviors.gd
@@ -3,13 +3,15 @@ extends Node
 class_name SteeringBehaviors
 
 var zombie
+var rng
 
 func _init(new_zombie) -> void:
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
 	zombie = new_zombie
-	
-func calculate() -> Vector2:
-	return arrive(Vector2(800, 500), Deceleration.normal)
 
+func calculate() -> Vector2:
+	return wander()
 
 #SEEK
 func seek(target_position: Vector2) -> Vector2:
@@ -28,12 +30,41 @@ func flee(target_position: Vector2) -> Vector2:
 #ARIVE
 enum Deceleration {slow = 5, normal = 2, fast = 1}
 const deceleration_tweaker: float = 2.0
-func arrive(target_position: Vector2, deceleration: int):
-	var vec_to_target: Vector2 = target_position - zombie.position
-	var distance = vec_to_target.length()
+func arrive(target_position: Vector2, deceleration: int) -> Vector2:
+	var to_target: Vector2 = target_position - zombie.position
+	var distance = to_target.length()
 	if distance > 0:
 		var speed: float = distance / (deceleration * deceleration_tweaker)
 		speed = min(speed, zombie.max_speed)
-		var desired_velocity: Vector2 = vec_to_target * speed / distance
+		var desired_velocity: Vector2 = to_target * speed / distance
 		return desired_velocity - zombie.velocity
 	return Vector2.ZERO
+
+#PURSUIT
+func pursuit(evader) -> Vector2:
+	var to_evader: Vector2 = evader.position - zombie.position
+	var relative_heading: float = zombie.heading.dot(evader.heading)
+	if to_evader.dot(zombie.heading) > 0 and relative_heading < -0.95:
+		return seek(evader.position)
+	var look_ahead_time: float = to_evader.length() / (zombie.max_speed + evader.velocity.length())
+	return seek(evader.position + evader.velocity * look_ahead_time)
+
+#EVADE
+func evade(pursuer) -> Vector2:
+	var to_pursuer = pursuer.position - zombie.position
+	var look_ahead_time = to_pursuer.length() / (zombie.max_speed + pursuer.velocity.length())
+	return flee(pursuer.position + pursuer.velocity * look_ahead_time)
+
+#WANDER
+var wander_radius: float = 300
+var wander_distance: float = 500
+var wander_jitter: float = 200
+var wander_target: Vector2 = Vector2.ZERO
+
+func wander() -> Vector2:
+	wander_target += Vector2(rng.randf_range(-1.0, 1.0) * wander_jitter, rng.randf_range(-1.0, 1.0) * wander_jitter)
+	wander_target = wander_target.normalized() * wander_radius
+	var target_local: Vector2 = wander_target + Vector2(wander_distance, 0)
+	var target_world = target_local.rotated(zombie.rotation) + zombie.position
+	return target_world - zombie.position
+
--- ZombieGame/Zombie.gd
+++ ZombieGame/Zombie.gd
@@ -3,18 +3,36 @@ extends Node2D
 
 var radius: int
 var velocity: Vector2
+var heading: Vector2
+var side: Vector2
 var steering: SteeringBehaviors
 var max_speed: float
 
+export var state = "seek"
+
 func _ready() -> void:
-	max_speed = 100
+	max_speed = 50
 	radius = 10
 	steering = SteeringBehaviors.new(self)
+	heading = Vector2.RIGHT
+	side = heading.rotated(PI/2)
 
 func _physics_process(delta) -> void:
-	velocity += steering.calculate() * delta
+	match state:
+		"seek":
+			velocity += steering.calculate() * delta
+		"pursuit":
+			velocity += steering.pursuit(get_parent().get_node("Zombie2")) * delta
+		"evade":
+			velocity += steering.evade(get_parent().get_node("Zombie3")) * delta
+		
 	velocity = velocity.clamped(max_speed)
 	position += velocity * delta
+	if velocity.length_squared() > 50:
+		heading = velocity.normalized()
+		side = heading.rotated(PI/2)
+		rotation = heading.angle()
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, radius, Color.red)
+	draw_circle(Vector2.ZERO, radius, Color.darkred)
+	draw_circle(radius*Vector2.RIGHT, 5, Color.darkred)
